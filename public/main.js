aebe6d37432cf5dfdc28c516ccc4bdbedd035e96
Laurynas <laurynas.tam@outlook.com>
2025-04-23 21:04:24 +0300

    Add a green sphere over vilnius representing defences

--- public/main.js
+++ public/main.js
@@ -9,3 +9,15 @@ const vilniusCoordinates = Cesium.Cartesian3.fromDegrees(25.2798, 54.68916, 0);
 viewer.camera.flyTo({
   destination: vilniusCoordinates,
 });
+
+const entities = viewer.entities;
+
+// This represents air defence over Vilnius
+const defenceRadius = 20000;
+entities.add({
+  position: vilniusCoordinates,
+  ellipsoid: {
+    radii: new Cesium.Cartesian3(defenceRadius, defenceRadius, defenceRadius),
+    material: new Cesium.Color(0, 1, 0, 0.15),
+  }
+});
