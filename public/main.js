abfbd8b865cf7c09a2305426670d79b56080aae2
Laurynas <laurynas.tam@outlook.com>
2025-04-23 19:40:48 +0300

    start at vilnius

--- public/main.js
+++ public/main.js
@@ -1,4 +1,11 @@
 Cesium.Ion.defaultAccessToken = window.CESIUM_TOKEN;
+
+/** @type {import("cesium").Viewer} */
 const viewer = new Cesium.Viewer('cesiumContainer', {
   terrain: Cesium.Terrain.fromWorldTerrain(),
+});
+
+const vilniusCoordinates = Cesium.Cartesian3.fromDegrees(25.2798, 54.68916, 0);
+viewer.camera.flyTo({
+  destination: vilniusCoordinates,
 });
