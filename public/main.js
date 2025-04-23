d71aaed9389a833aeda46031bdc87fd31ba42180
Laurynas <laurynas.tam@outlook.com>
2025-04-23 21:49:33 +0300

    Add actions to place air defences

--- public/main.js
+++ public/main.js
@@ -32,22 +32,71 @@ const addAirDefence = (type, longitude, latitude, radius) => {
     ellipsoid: {
       radii: new Cesium.Cartesian3(radius, radius, radius),
       material: color,
+    },
+    properties: {
+      isAirDefence: true
     }
   });
 };
 
 addAirDefence("friendly", 25.2798, 54.68916, 30000);
 
-// Actions
+// UI Selectors
+const actionPicker = document.getElementById("actionPicker");
+const typePicker = document.getElementById("typePicker");
+const radiusInput = document.getElementById("radiusInput");
 
-viewer.screenSpaceEventHandler.setInputAction((click) => {
-  const pickedPosition = viewer.scene.pickPosition(click.position);
-  
-  if (Cesium.defined(pickedPosition)) {
-    const cartographic = Cesium.Cartographic.fromCartesian(pickedPosition);
-    const lon = Cesium.Math.toDegrees(cartographic.longitude);
-    const lat = Cesium.Math.toDegrees(cartographic.latitude);
+const actionControls = {
+  addAirDefence: document.getElementById("addAirDefenceControl"),
+};
 
-    addAirDefence("enemy", lon, lat, 20000);
+const updateUIVisibility = () => {
+  for (const controlName in actionControls) {
+    const control = actionControls[controlName];
+    control.style.display = "none";
   }
+
+  const selected = actionPicker.value;
+  control = actionControls[selected];
+  if (control)
+    control.style.display = "inline-block";
+}
+
+actionPicker.addEventListener("change", updateUIVisibility);
+
+// Actions
+const actions = {
+  addAirDefence: (click) => {
+    const pickedPosition = viewer.scene.pickPosition(click.position);
+
+    if (Cesium.defined(pickedPosition)) {
+      const cartographic = Cesium.Cartographic.fromCartesian(pickedPosition);
+      const lon = Cesium.Math.toDegrees(cartographic.longitude);
+      const lat = Cesium.Math.toDegrees(cartographic.latitude);
+  
+      let radius = 20000;
+      try {
+        radius = parseFloat(radiusInput.value);
+      } catch {
+        console.log("failed to parse radius input");
+      }
+
+      addAirDefence(typePicker.value, lon, lat, radius);
+    }
+  },
+  deleteEntity: (click) => {
+    console.log("Delete");
+    const pickedObject = viewer.scene.pick(click.position);
+    if (Cesium.defined(pickedObject) && pickedObject.id?.properties?.isAirDefence)
+      entities.remove(pickedObject.id);
+  }
+}
+
+// Bindings
+viewer.screenSpaceEventHandler.setInputAction((click) => {
+  const actionName = actionPicker.value;
+  const action = actions[actionName];
+  if (action)
+    action(click);
+
 }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
